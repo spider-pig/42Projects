2ce82cb38a620e70f80c2b84e34bea29fa021a4f
cleticia <carlalrfranca@gmail.com>
2021-06-18 21:57:17 -0300

    ok

---
+++ get_next_line.h
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cleticia <cleticia@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/16 16:20:39 by cleticia          #+#    #+#             */
+/*   Updated: 2021/06/17 19:39:34 by cleticia         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+# define R_LINE 1
+# define R_EOF 0
+# define R_ERROR -1
+
+int		get_next_line(int fd, char **line);
+
+
+ssize_t	ft_strlen(const char *s);
+char	*ft_strdup(const char *s1);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strchr(const char *s, int c);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+void	ft_strcpy(char *dst, const char *src, size_t dstsize);
+
+#endif
