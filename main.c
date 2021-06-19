fb07b61382c5d495a3367ad5c8cc218e2fa31826
cleticia <carlalrfranca@gmail.com>
2021-06-18 21:59:17 -0300

    ok

---
+++ main.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cleticia <cleticia@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/16 16:40:37 by cleticia          #+#    #+#             */
+/*   Updated: 2021/06/17 19:57:08 by cleticia         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+#include <stdio.h>
+#include <fcntl.h>
+#define BUFFER_SIZE 50
+
+int main()
+{
+    char *linha;
+    int fd;
+    int ret;
+    
+    fd = open("/home/usuario/get-next-line/texto.txt", O_RDONLY);
+    while (ret == 1)
+    {
+        ret = get_next_line(fd, &linha);
+        printf("%s", linha);
+        free(linha);   
+    }
+    return (0);
+}
+
+// vai chamar get_next_line até ler todo o arquivo
+// a main vai ter que chamar a função todas vezes para ler linha por linha
+// o loop depende da resposta da get-next-0line que pe o inteiro que ela retorna
+// a main em ais um inteiro para armazenar o numero que o gnl retorna para a main
+// ela chama a gnl até que de como repsosta uminteiro 0
+// a main precisa ter um loop
+/*
+
+/*
+
+
+*/
