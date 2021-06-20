859a8f43acc58090b9eee90e4090965355301b43
Carla França <71091956+CarlalrFranca@users.noreply.github.com>
2021-06-20 19:53:56 -0300

    testing

--- get_next_line.c
+++ get_next_line.c
@@ -12,148 +12,111 @@
 
 #include "get_next_line.h"
 
-static int	check_errors(int fd, char **line, t_gnl *string)
+static int read_process(int fd, char **line, char **save);
+static int join_line_and_save(char **line, char **save);
+static int join_line_and_buf(char **line, char *buf);
+static int split_by_newline(char **line, char **save, char *buf);
+int get_next_line(int fd, char **line);
+
+static int read_process(int fd, char **line, char **save)
 {
-	ft_memset(string, 0, sizeof(*string));
-	if ((fd < 0) || (fd > RLIMIT_NOFILE) || (BUFFER_SIZE < 1) || !(line))
-		return (1);
-	return (0);
+    ssize_t        read_size;
+    int            ret;
+    char       *buf;
+
+    ret = CONTINUE_READ;
+    if (!(buf = malloc(BUFFER_SIZE + 1)))
+        return (ERROR);
+    while (ret == CONTINUE_READ && (read_size = read(fd, buf, BUFFER_SIZE)) > 0)
+    {
+        buf[read_size] = '\0';
+        if (ft_strchr(buf, '\n'))
+            ret = split_by_newline(line, save, buf);
+        else
+            ret = join_line_and_buf(line, buf);
+    }
+    free(buf);
+    if (ret == CONTINUE_READ && read_size == 0)
+        ret = END_OF_FILE;
+    else if (ret == CONTINUE_READ && read_size == -1)
+        ret = ERROR;
+    return (ret);
 }
 
-static int	add_to_line(t_gnl *string, char **line, char **excess, int new_line)
+static int join_line_and_save(char **line, char **save)
 {
-	if (new_line)
-		*string->break_line_ptr = '\0';
-	string->read[string->read_return] = '\0';
-	string->temp = ft_strjoin(*line, string->read);
-	if (string->temp == NULL)
-		return (ERROR);
-	free(*line);
-	*line = string->temp;
-	if (new_line)
-	{
-		string->temp = ft_strdup(string->break_line_ptr + 1);
-		if (string->temp == NULL)
-			return (ERROR);
-		free(*excess);
-		*excess = string->temp;
-	}
-	return (READ_LINE);
+    char   *tmp;
+    char   *newline_ptr;
+
+    if ((newline_ptr = ft_strchr(*save, '\n')))
+    {
+        tmp = *line;
+        *line = ft_substr(*save, 0, newline_ptr - *save);
+        free(tmp);
+        if (!(*line))
+            return (ERROR);
+        tmp = *save;
+        *save = ft_substr(newline_ptr + 1, 0, ft_strlen(newline_ptr + 1));
+        free(tmp);
+        if (!(*save))
+            return (ERROR);
+        return (SUCCESS);
+    }
+    else
+    {
+        tmp = *line;
+        *line = *save;
+        *save = NULL;
+        free(tmp);
+        return (CONTINUE_READ);
+    }
 }
 
-static int	add_excess(char **line, t_gnl *string, char **excess, int new_line)
+static int join_line_and_buf(char **line, char *buf)
 {
-	if (new_line)
-		*string->break_line_ptr = '\0';
-	string->temp = ft_strjoin(*line, *excess);
-	if (string->temp == NULL)
-		return (ERROR);
-	free(*line);
-	*line = string->temp;
-	if (new_line)
-	{
-		string->temp = ft_strdup(string->break_line_ptr + 1);
-		if (string->temp == NULL)
-			return (ERROR);
-	}
-	free(*excess);
-	if (new_line)
-		*excess = string->temp;
-	else
-		*excess = NULL;
-	return (READ_LINE);
+    char   *tmp;
+
+    tmp = *line;
+    *line = ft_strjoin(*line, buf);
+    free(tmp);
+    if (!(*line))
+        return (ERROR);
+    return (CONTINUE_READ);
 }
 
-int			get_next_line(int fd, char **line)
+static int split_by_newline(char **line, char **save, char *buf)
 {
-	static char		*excess;
-	t_gnl			string;
+    char   *old_line;
+    char   *tmp;
+    char   *newline_ptr;
 
-	if ((check_errors(fd, line, &string)) || (*line = ft_strdup("")) == NULL)
-		return (-1);
-	if (excess != NULL)
-	{
-		if ((string.break_line_ptr = ft_strchr(excess, '\n')))
-			return (add_excess(line, &string, &excess, NEW_LINE));
-		if ((add_excess(line, &string, &excess, NO_NEW_LINE)) == ERROR)
-			return (ERROR);
-	}
-	while (((string.read_return = read(fd, string.read, BUFFER_SIZE)) > 0)
-			&& !(string.break_line_ptr = ft_strchr(string.read, '\n')))
-		if (add_to_line(&string, line, &excess, NO_NEW_LINE) == ERROR)
-			return (ERROR);
-	if (string.read_return < 1)
-	{
-		free(excess);
-		return (string.read_return);
-	}
-	return (add_to_line(&string, line, &excess, NEW_LINE));
-}
-	i = -1;
-	while (++i < size)
-		*(ptr + i) = 0;
-	return (ptr);
+    newline_ptr = ft_strchr(buf, '\n');
+    if (!(tmp = ft_substr(buf, 0, newline_ptr - buf)))
+        return (ERROR);
+    old_line = *line;
+    *line = ft_strjoin(*line, tmp);
+    free(old_line);
+    free(tmp);
+    if (!(*line))
+        return (ERROR);
+    if (!(*save = ft_substr(newline_ptr + 1, 0,
+                                ft_strlen(newline_ptr + 1))))
+        return (ERROR);
+    return (SUCCESS);
 }
 
-static	int	find_nl(int fd, char **buf, char **line)
+
+int            get_next_line(int fd, char **line)
 {
-	char		*new_line;
-	char		*tmp;
+    int            ret;
+    static char   *save;
 
-	new_line = ft_strchr(buf[fd], '\n');
-	while (new_line)
-	{
-		tmp = buf[fd];
-		*new_line = '\0';
-		*line = ft_substr(buf[fd], 0, new_line - buf[fd]);
-		buf[fd] = ft_strdup(new_line + 1);
-		free(tmp);
-		return (1);
-	}
-	while (*buf[fd])
-	{
-		*line = (char *)malloc(sizeof(char) * (ft_strlen(buf[fd] + 1)));
-		*line =  ft_strdup(buf[fd]);
-		buf[fd] = zelloc(BUFFER_SIZE + 1);
-		return (1);
-	}
-	return (0);
-}
-
-static	int	ft_read(int fd, char **line)
-{
-	char		*buf;
-	char		*tmp;
-	int			i;
-
-	buf = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
-	while ((i = read(fd, buf, BUFFER_SIZE)) > 0)
-	{
-		if (!line[fd])
-		{
-			line[fd] = (char *)malloc(sizeof(char) * (ft_strlen(buf) + 1));
-			line[fd] = ft_strdup(&buf[fd]);
-		}
-		else
-		{
-			tmp = line[fd];
-			line[fd] = ft_strjoin(line[fd], buf);
-			free(tmp);
-		}
-		setter(buf, 0, BUFFER_SIZE);		
-	}
-	free(buf);
-	return (i);
-}
-
-int			get_next_line(const int fd, char **line)
-{
-	static	char	*buf[FLDS];
-
-	if (!line || fd < 0 || BUFFER_SIZE < 0
-	||(ft_read(fd, &buf[fd]) < 0) || fd > FLDS)
-		return (-1);
-	if (find_nl(fd, &buf[fd], line) == 1)
-		return (1);
-	return (0);
+    if (fd < 0 || !line || BUFFER_SIZE <= 0 || !(*line = ft_strdup("")))
+        return (ERROR);
+    ret = CONTINUE_READ;
+    if (save)
+        ret = join_line_and_save(line, &save);
+    if (ret == CONTINUE_READ)
+        ret = read_process(fd, line, &save);
+    return (ret);
 }
