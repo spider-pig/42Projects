708d1e65d52bff362fabcb16b68acb6b9ff28c66
user42 <carlalrfranca@gmail.com>
2021-06-20 18:21:53 -0300

    OK

--- get_next_line.h
+++ get_next_line.h
@@ -6,15 +6,20 @@
 /*   By: cleticia <cleticia@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/16 16:20:39 by cleticia          #+#    #+#             */
-/*   Updated: 2021/06/17 19:39:34 by cleticia         ###   ########.fr       */
+/*   Updated: 2021/06/20 18:17:55 by cleticia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+# define BUFFER_SIZE 50
+# define FLDS 65535
 
-# include <stdlib.h>
-# include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
 
 # define R_LINE 1
 # define R_EOF 0
@@ -22,13 +27,11 @@
 
 int		get_next_line(int fd, char **line);
 
-
-ssize_t	ft_strlen(const char *s);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+size_t	ft_strlen(const char *s);
 char	*ft_strdup(const char *s1);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strchr(const char *s, int c);
-char	*ft_substr(char const *s, unsigned int start, size_t len);
-size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
-void	ft_strcpy(char *dst, const char *src, size_t dstsize);
+
 
 #endif
