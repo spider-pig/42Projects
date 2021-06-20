cd3b57acea6ed0efd515383a2d3b64d257584e59
Carla França <71091956+CarlalrFranca@users.noreply.github.com>
2021-06-20 19:59:04 -0300

    testing OK

--- get_next_line.h
+++ get_next_line.h
@@ -9,23 +9,22 @@
 /*   Updated: 2021/06/20 18:17:55 by cleticia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
 # include <stdlib.h>
 # include <unistd.h>
 
-# define SUCCESS       1
-# define END_OF_FILE   0
-# define ERROR         -1
-# define CONTINUE_READ -2
+# define SUCCESS		1
+# define END_OF_FILE	0
+# define ERROR			-1
+# define CONTINUE_READ	-2
 
-# ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 256
-# endif
-
-int        get_next_line(int fd, char **line);
-char   *ft_strdup(const char *s);
-char   *ft_substr(char const *s, unsigned int start, size_t len);
-size_t ft_strlen(const char *s);
-char   *ft_strchr(const char *s, int c);
-char   *ft_strjoin(char const *s1, char const *s2);
+int		get_next_line(int fd, char **line);
+char	*ft_strdup(const char *s);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+size_t	ft_strlen(const char *s);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(char const *s1, char const *s2);
 
 #endif
