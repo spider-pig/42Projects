e5337fc71083d2a095a016a97e17995e7d013859
Carla França <71091956+CarlalrFranca@users.noreply.github.com>
2021-06-24 09:37:04 -0300

    atualization

--- get_next_line.h
+++ get_next_line.h
@@ -9,27 +9,25 @@
 /*   Updated: 2021/06/20 22:17:27 by cleticia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-
-# include <stdlib.h>
 # include <unistd.h>
+# include <fcntl.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <stddef.h>
+# define BUFFER_SIZE	32
 
-# define R_LINE 1
-# define R_EOF 0
-# define R_ERROR -1
+char	*ft_strdup(char *src);
+
+char	*ft_strjoin(char const *s1, char const *s2);
+
+void	*ft_calloc(size_t nmemb, size_t size);
+
+size_t	ft_strlen(char const *str);
+
+void	*ft_memccpy(void *dest, void *src, int c, size_t n);
 
 int		get_next_line(int fd, char **line);
-
-size_t	ft_strlen(const char *s);
-void	ft_strcpy(char *dst, const char *src, size_t dstsize);
-char	*ft_strdup(const char *s1);
-char	*ft_substr(char const *s, unsigned int start, size_t len);
-
-int		findchr(const char *s, char c);
-char	*strjoin_free(char *s1, char *s2);
-int		strcpy_line_bl(char **line, char *str, int i, char *buff);
-int		get_read(int fd, char **line, char *buff, char **str);
 
 #endif
