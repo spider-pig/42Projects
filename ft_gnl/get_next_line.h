146d3042a6204abe0b95320f4282ec53cd768959
Carla R. França <71091956+carlarfranca@users.noreply.github.com>
2021-10-12 11:20:08 -0300

    update

--- ft_gnl/get_next_line.h
+++ ft_gnl/get_next_line.h
@@ -6,31 +6,23 @@
 /*   By: cleticia <cleticia@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/16 16:20:39 by cleticia          #+#    #+#             */
-/*   Updated: 2021/06/20 22:17:27 by cleticia         ###   ########.fr       */
+/*   Updated: 2021/10/12 05:22:55 by cleticia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# include <unistd.h>
-# include <fcntl.h>
 # include <stdlib.h>
-# include <stdio.h>
-# include <stddef.h>
+# include <unistd.h>
+# include <limits.h>
 
-# define R_LINE 1
-# define R_EOF 0
-# define R_ERROR -1
-
-char	*ft_strdup(char *src);
-
-char	*ft_strjoin(char const *s1, char const *s2);
-
-void	*ft_calloc(size_t nmemb, size_t size);
-
-size_t	ft_strlen(char const *str);
-
-void	*ft_memccpy(void *dest, void *src, int c, size_t n);
+# define BUFFER_SIZE 10
 
 int		get_next_line(int fd, char **line);
+char	*ft_strdup(const char *src);
+char	*ft_strchr(const char *s, int c);
+size_t	ft_strlen(char const *str);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
 
 #endif
